2d145c7255e966f6d0b5d4d04aba5c6ba403a3b3
unknown <nasrulmnf@gmail.com>
2024-06-05 10:25:34 +0800

    Git init

--- resources/views/livewire/post/post-list.blade.php
+++ resources/views/livewire/post/post-list.blade.php
@@ -4,13 +4,13 @@
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
-                    Id
+                    Bil
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Title
+                    Tajuk
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Content
+                    Kadungan
                 </th>
                 <th scope="col" class="px-6 py-3">
                     Action
