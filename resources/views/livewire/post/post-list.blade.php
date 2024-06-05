2cfee4de2a060d885c4c3e899bb4bbe7716307d5
Nadiah <nqwerty95@gmail.com>
2024-06-05 10:03:22 +0800

    Git init

---
+++ resources/views/livewire/post/post-list.blade.php
@@ -0,0 +1,41 @@
+<div>
+
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Id
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Title
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Content
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $post->id }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ $post->title }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $post->content }}
+                </td>
+                <td class="px-6 py-4">
+                    Edit / View / Delete
+                </td>
+            </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+
+</div>
